c45450be8705b2b8c29cd3fbe091a935b3bf2048
yosefbeder <dryosefbeder@gmail.com>
2022-05-05 07:33:29 +0200

    feat(cli): make the cli in Arabic

--- src/reporter.rs
+++ src/reporter.rs
@@ -43,3 +43,34 @@ pub trait Reporter<'a> {
     fn warning(&mut self, report: Report<'a>);
     fn error(&mut self, report: Report<'a>);
 }
+
+pub struct CliReporter {
+    errors_count: usize,
+    warnings_count: usize,
+}
+
+impl CliReporter {
+    pub fn new() -> Self {
+        Self {
+            errors_count: 0,
+            warnings_count: 0,
+        }
+    }
+}
+
+impl<'a> Reporter<'a> for CliReporter {
+    fn warning(&mut self, report: Report) {
+        self.warnings_count += 1;
+        let (line, col) = report.token.get_pos();
+        println!("تحذير: {} [{}:{}]\n{}", report.msg, line, col, report.token);
+    }
+
+    fn error(&mut self, report: Report) {
+        self.errors_count += 1;
+        let (line, col) = report.token.get_pos();
+        eprintln!(
+            "خطأ {}: {} [{}:{}]\n{}",
+            report.phase, report.msg, line, col, report.token
+        );
+    }
+}
